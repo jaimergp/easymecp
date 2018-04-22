22f3c07f4099d5d252c86c828f5d49e094abd00e
Jaime RG <jaime.rogue@gmail.com>
2018-04-22 03:01:49 +0200

    add some base tests

---
+++ tests/test_mecpsh.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import pytest
+from mecpsh.mecpsh import MECPCalculation, temporary_directory
+here = os.path.abspath(os.path.dirname(__file__))
+
+@pytest.mark.parametrize("directory",
+sorted(os.listdir(os.path.join(here, 'data')))[0:1]
+)
+def test_mecpsh(directory):
+    cwd = os.getcwd()
+    with temporary_directory() as tmp:
+        shutil.copytree(os.path.join(here, 'data', directory), os.path.join(tmp, directory))
+        os.chdir(directory)
+        calc = MECPCalculation(geom='geom_init')
+        result = calc.run()
+        assert result == calc.OK
