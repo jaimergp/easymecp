ed80a33c1676a110208f420f6d650052c0092071
Jaime RG <jaime.rogue@gmail.com>
2018-04-22 03:08:24 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+import os
+import io
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(os.path.join(here, filename), encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+setup(
+    name='mecpy',
+    version='0.1',
+    url='https://github.com/jaimergp/mecpy',
+    license='MIT License',
+    author='Jaime RGP',
+    author_email='jaime.rogue@gmail.com',
+    description='Python utilities for performing MECP (Minimum Energy Crossing Point) with Gaussian.',
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Chemistry',
+    ],
+    entry_points='''
+        [console_scripts]
+        mecpsh=mecpsh.mecpsh:main
+        '''
+    ,
+)
