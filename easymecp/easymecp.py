10aa7ef4435ab38b9b2d6d9742c5a4e4582a8e43
Jaime RG <jaime.rogue@gmail.com>
2018-06-05 16:18:22 +0200

    Style fixes

--- easymecp/easymecp.py
+++ easymecp/easymecp.py
@@ -614,8 +614,10 @@ def main():
 # Constants
 ###
 DEFAULTS = _get_defaults()
+
 AVAILABLE_ENERGY_PARSERS = set([key[14:] for key in globals().copy()
                                 if key.startswith('_parse_energy_')])
+
 PROGFILE = """
 Title
 Number of Atoms
@@ -627,42 +629,69 @@ Is this a complete ProgFile ?
 Geometry:
 {geometry}
 """
+
 ELEMENTS = {
-    'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9,
-    'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17,
-    'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25,
-    'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33,
-    'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41,
-    'Mo': 42, 'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49,
-    'Sn': 50, 'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57,
-    'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65,
-    'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73,
-    'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81,
-    'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89,
-    'Th': 90, 'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97,
-    'Cf': 98, 'Es': 99, 'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104,
-    'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111,
-    'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118
+    'H':    1,  'He':   2,  'Li':   3,  'Be':   4,  'B':    5,
+    'C':    6,  'N':    7,  'O':    8,  'F':    9,  'Ne':  10,
+    'Na':  11,  'Mg':  12,  'Al':  13,  'Si':  14,  'P':   15,
+    'S':   16,  'Cl':  17,  'Ar':  18,  'K':   19,  'Ca':  20,
+    'Sc':  21,  'Ti':  22,  'V':   23,  'Cr':  24,  'Mn':  25,
+    'Fe':  26,  'Co':  27,  'Ni':  28,  'Cu':  29,  'Zn':  30,
+    'Ga':  31,  'Ge':  32,  'As':  33,  'Se':  34,  'Br':  35,
+    'Kr':  36,  'Rb':  37,  'Sr':  38,  'Y':   39,  'Zr':  40,
+    'Nb':  41,  'Mo':  42,  'Tc':  43,  'Ru':  44,  'Rh':  45,
+    'Pd':  46,  'Ag':  47,  'Cd':  48,  'In':  49,  'Sn':  50,
+    'Sb':  51,  'Te':  52,  'I':   53,  'Xe':  54,  'Cs':  55,
+    'Ba':  56,  'La':  57,  'Ce':  58,  'Pr':  59,  'Nd':  60,
+    'Pm':  61,  'Sm':  62,  'Eu':  63,  'Gd':  64,  'Tb':  65,
+    'Dy':  66,  'Ho':  67,  'Er':  68,  'Tm':  69,  'Yb':  70,
+    'Lu':  71,  'Hf':  72,  'Ta':  73,  'W':   74,  'Re':  75,
+    'Os':  76,  'Ir':  77,  'Pt':  78,  'Au':  79,  'Hg':  80,
+    'Tl':  81,  'Pb':  82,  'Bi':  83,  'Po':  84,  'At':  85,
+    'Rn':  86,  'Fr':  87,  'Ra':  88,  'Ac':  89,  'Th':  90,
+    'Pa':  91,  'U':   92,  'Np':  93,  'Pu':  94,  'Am':  95,
+    'Cm':  96,  'Bk':  97,  'Cf':  98,  'Es':  99,  'Fm': 100,
+    'Md': 101,  'No': 102,  'Lr': 103,  'Rf': 104,  'Db': 105,
+    'Sg': 106,  'Bh': 107,  'Hs': 108,  'Mt': 109,  'Ds': 110,
+    'Rg': 111,  'Cn': 112,  'Nh': 113,  'Fl': 114,  'Mc': 115,
+    'Lv': 116,  'Ts': 117,  'Og': 118
 }
+
 REVERSE_ELEMENTS = dict((v, k) for (k, v) in ELEMENTS.items())
+
 USAGE = {
-    'max_steps': 'Number of iterations to perform until stop or convergence',
-    'natom': 'If easymecp cannot detect number of atoms automatically, override with this option',
-    'a_header': 'File containing the top part of system configuration with multiplicity A',
-    'b_header': 'File containing the top part of system configuration with multiplicity B',
-    'geom': 'File containing the starting system geometry '
-            '(element symbols will be converted in atomic numbers automatically)',
-    'footer': 'File containing the bottom part of system configuration',
-    'energy_parser': 'Which energy should be parsed: dft, mp2, cis, td. It can also be a '
-                     'path to a Python file containing a `parse_energy` function.',
-    'gaussian_exe': 'Path to gaussian executable. Compatible versions: g09, g16',
-    'TDE': 'Convergence threshold for difference in E. Must be a valid Fortran double!',
-    'TDXMax': 'Convergence threshold for max change of X. Must be a valid Fortran double!',
-    'TDXRMS': 'Convergence threshold for RMS change of X. Must be a valid Fortran double!',
-    'TGMax': 'Convergence threshold for max gradient el. Must be a valid Fortran double!',
-    'TGRMS': 'Convergence threshold for rms gradient el. Must be a valid Fortran double!',
-    'FC': 'Fortran compiler (can also be set with $FC environment variable)',
-    'FFLAGS': 'Fortran compiler flags (can also be set with $FFLAGS environment variable)'
+    'max_steps':
+       'Number of iterations to perform until stop or convergence',
+    'natom':
+        'If easymecp cannot detect number of atoms automatically, override with this option',
+    'a_header':
+        'File containing the top part of system configuration with multiplicity A',
+    'b_header':
+        'File containing the top part of system configuration with multiplicity B',
+    'geom':
+        'File containing the starting system geometry '
+        '(element symbols will be converted in atomic numbers automatically)',
+    'footer':
+        'File containing the bottom part of system configuration',
+    'energy_parser':
+        'Which energy should be parsed: dft, mp2, cis, td. It can also be a '
+        'path to a Python file containing a `parse_energy` function.',
+    'gaussian_exe':
+        'Path to gaussian executable. Compatible versions: g09, g16',
+    'TDE':
+        'Convergence threshold for difference in E. Must be a valid Fortran double!',
+    'TDXMax':
+        'Convergence threshold for max change of X. Must be a valid Fortran double!',
+    'TDXRMS':
+        'Convergence threshold for RMS change of X. Must be a valid Fortran double!',
+    'TGMax':
+        'Convergence threshold for max gradient el. Must be a valid Fortran double!',
+    'TGRMS':
+        'Convergence threshold for rms gradient el. Must be a valid Fortran double!',
+    'FC':
+        'Fortran compiler (can also be set with $FC environment variable)',
+    'FFLAGS':
+        'Fortran compiler flags (can also be set with $FFLAGS environment variable)'
 }
 
 MECP_FORTRAN = """
@@ -703,7 +732,7 @@ C          Checks Delta E, X, Mag. Delta G. Writes Output File
        SUBROUTINE Effective_Gradient(N,Ea,Eb,Ga,Gb,ParG,PerpG,G)
        implicit none
 
-C      Computes the parallel and perpendicular compenents of the Effective Gradient,
+C      Computes the parallel and perpendicular components of the Effective Gradient,
 C      As well as the effective gradient itself.
 
        integer n, i
